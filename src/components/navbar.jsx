a26f65189c95574b02648544856a2614d2d6b4a7
Mahdi <mmahdikarimi2006@gmail.com>
2024-08-11 17:19:18 +0430

    navbar structur designed

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,14 +1,44 @@
 import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClose, faNavicon } from "@fortawesome/free-solid-svg-icons";
+
+const menu = [
+  { id: 0, name: "home", link: "/" },
+  { id: 1, name: "shop", link: "/shop" },
+  { id: 2, name: "about", link: "/about" },
+  { id: 3, name: "contact", link: "/contact" },
+];
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <nav className="absolute top-0 left-0 w-full h-36 px-20 bg-transparent z-50 flex items-center gap-14">
-      <img src="/images/logo.svg" className="w-18 sm:hidden" />
-      <ul className="gap-10 sm:text-black sm:bg-white text-white font-semibold hidden lg:flex xl:flex list">
-        <li href="#">home</li>
-        <li href="#shop">shop</li>
-        <li href="#about">about</li>
-        <li href="#contact">contact</li>
+    <nav
+      className={`absolute top-0 left-0 w-full h-32 px-8 sm:px-24 bg-transparent text-white z-50 items-center grid grid-cols-3 sm:flex gap-8 sm:gap-10 lg:gap-16`}
+    >
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        className={`sm:hidden justify-self-start ${isOpen && "text-black"}`}
+      >
+        <FontAwesomeIcon icon={faNavicon} className="nav-btn" />
+      </button>
+      <img
+        src="/images/logo.svg"
+        className={`w-20 mx-auto sm:mx-0 ${
+          isOpen ? "hidden sm:block" : "none"
+        }`}
+      />
+      <ul
+        className={`gap-8 font-semibold h-8 ${
+          isOpen ? "flex text-black sm:text-white" : "hidden sm:flex"
+        }`}
+      >
+        {menu.map((item) => (
+          <a key={item.id} href={item.link} className="relative group">
+            <div>{item.name}</div>
+            <div className="list-item"></div>
+          </a>
+        ))}
       </ul>
     </nav>
   );
