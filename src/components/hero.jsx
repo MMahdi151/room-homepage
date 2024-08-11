9597962ce54ad7305409c4e24752c855efad6265
Mahdi <mmahdikarimi2006@gmail.com>
2024-08-11 17:07:55 +0430

    hero structure designed

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,35 +1,41 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronLeft,
+  faChevronRight,
+  faArrowRightLong,
+} from "@fortawesome/free-solid-svg-icons";
+
 export default function Hero() {
   return (
-    <section className="">
-      <div className="w-screen h-fit grid grid-cols-1 lg:grid-cols-[6fr_4fr] xl:grid-cols-[6fr_4fr]">
-        <div className="bg-green-400 relative">
-          <div className="flex items-center justify-center">
-            <img
-              src="/images/desktop-image-hero-1.jpg"
-              className="hidden md:block lg:block xl:block w-full"
-            />
-            <img
-              src="/images/mobile-image-hero-1.jpg"
-              className="block md:hidden lg:hidden xl:hidden w-full"
-            />
-          </div>
-          <div className="h-24 w-44 bg-gray-950 text-gray-300 absolute bottom-0 right-0 flex items-stretch xl:translate-x-full lg:translate-x-full">
-            <div className="slide-btn">&gt;</div>
-            <div className="slide-btn">&lt;</div>
-          </div>
+    <section>
+      <div className="w-screen grid grid-cols-1 md:grid-cols-[6fr_4fr]">
+        <div className="flex h-full">
+          <img
+            src="/images/desktop-image-hero-1.jpg"
+            className="hidden md:block object-cover w-full"
+          />
         </div>
-        <div className="lg:py-28 lg:px-24 xl:py-28 xl:px-24 px-8 py-12 flex flex-col gap-8">
-          <h1 className="font-bold text-5xl tracking-tight">
+        <div className="h-14 w-28 sm:h-24 sm:w-44 absolute bottom-0 right-0 flex md:translate-x-full">
+          <button className="slide-btn">
+            <FontAwesomeIcon icon={faChevronLeft} />
+          </button>
+          <button className="slide-btn">
+            <FontAwesomeIcon icon={faChevronRight} />
+          </button>
+        </div>
+        <div className="max-sm:py-16 px-8 py-32 lg:px-20 lg:py-36 flex flex-col gap-4 lg:gap-6">
+          <h1 className="font-bold text-4xl lg:text-5xl tracking-tight">
             Discover innovative ways to decorate
           </h1>
-          <p className="text-gray-500">
+          <p className="p">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
             delectus saepe, sint harum voluptas quasi voluptatum architecto.
             Neque, excepturi ratione enim ad adipisci soluta nostrum numquam
             distinctio, eaque sit iure.
           </p>
-          <a className="uppercase tracking-[12px] font-semibold">
-            Shop Now ---
+          <a href="#" className="shop-now">
+            Shop Now
+            <FontAwesomeIcon icon={faArrowRightLong} />
           </a>
         </div>
       </div>
