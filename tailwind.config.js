f4b5e2671b7e1e6c25a92667ba18dc5c4efbaaad
Mahdi <mmahdikarimi2006@gmail.com>
2024-08-11 13:53:52 +0430

    default colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "hsl(0, 0%, 100%)",
+        gray: "hsl(0, 0%, 63%)",
+        darkgray: "hsl(0, 0%, 27%)",
+        black: "hsl(0, 0%, 0%)",
+      },
+    },
   },
   plugins: [],
 };
