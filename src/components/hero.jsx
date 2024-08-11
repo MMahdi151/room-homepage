a53ad931f8834fadf10341c471491485cd424122
Mahdi <mmahdikarimi2006@gmail.com>
2024-08-11 17:23:15 +0430

    hero responsive designed

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -9,19 +9,25 @@ export default function Hero() {
   return (
     <section>
       <div className="w-screen grid grid-cols-1 md:grid-cols-[6fr_4fr]">
-        <div className="flex h-full">
-          <img
-            src="/images/desktop-image-hero-1.jpg"
-            className="hidden md:block object-cover w-full"
-          />
-        </div>
-        <div className="h-14 w-28 sm:h-24 sm:w-44 absolute bottom-0 right-0 flex md:translate-x-full">
-          <button className="slide-btn">
-            <FontAwesomeIcon icon={faChevronLeft} />
-          </button>
-          <button className="slide-btn">
-            <FontAwesomeIcon icon={faChevronRight} />
-          </button>
+        <div className="relative">
+          <div className="flex h-full">
+            <img
+              src="/images/desktop-image-hero-1.jpg"
+              className="hidden md:block object-cover w-full"
+            />
+            <img
+              src="/images/mobile-image-hero-1.jpg"
+              className="block md:hidden w-full"
+            />
+          </div>
+          <div className="h-14 w-28 sm:h-24 sm:w-44 absolute bottom-0 right-0 flex md:translate-x-full">
+            <button className="slide-btn">
+              <FontAwesomeIcon icon={faChevronLeft} />
+            </button>
+            <button className="slide-btn">
+              <FontAwesomeIcon icon={faChevronRight} />
+            </button>
+          </div>
         </div>
         <div className="max-sm:py-16 px-8 py-32 lg:px-20 lg:py-36 flex flex-col gap-4 lg:gap-6">
           <h1 className="font-bold text-4xl lg:text-5xl tracking-tight">
