3594a819fd180a361d8b3985f421e03be7fe8d16
Mahdi <mmahdikarimi2006@gmail.com>
2024-08-11 18:18:40 +0430

    about structure designed

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,14 +1,17 @@
 export default function About() {
   return (
     <section className="w-screen grid grid-cols-1 lg:grid-cols-[2fr_3fr_2fr] xl:grid-cols-[2fr-3fr-2fr]">
-      <div>
-        <img src="/images/image-about-dark.jpg" />
+      <div className="aspect-video sm:aspect-auto w-auto">
+        <img
+          src="/images/image-about-dark.jpg"
+          className="object-cover w-full h-full"
+        />
       </div>
       <div className="px-8 py-12">
-        <h1 className="uppercase mb-4 font-bold tracking-[8px]">
+        <h1 className="uppercase mb-4 font-bold tracking-[5px]">
           About Our Furniture
         </h1>
-        <p className="text-gray-500">
+        <p className="p">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat,
           repellendus expedita? Est dicta cum rem tempora error sint fuga eos
           ipsum at, nam iste distinctio consectetur corporis laboriosam totam
@@ -16,8 +19,11 @@ export default function About() {
           elit. Similique soluta et enim, vero aliquid non dolores!
         </p>
       </div>
-      <div>
-        <img src="/images/image-about-light.jpg" />
+      <div className="acpect-video sm:aspect-auto w-auto">
+        <img
+          src="/images/image-about-light.jpg"
+          className="object-cover w-full h-full"
+        />
       </div>
     </section>
   );
